0bf566fa15848429bdd0ee5b273dbb83a2909f13
Jose Bocanegra <j.bocanegra@uniandes.edu.co>
2024-08-01 11:44:28 -0500

    Add files1

--- semana1-conceptosbasicos/index.php
+++ semana1-conceptosbasicos/index.php
@@ -1,28 +1,28 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "neptuno";
+  $servername = "localhost";
+  $username = "root";
+  $password = "";
+  $dbname = "neptuno";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+  // Create connection
+  $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-echo "Connected successfully";
-
-$sql = "SELECT * FROM clientes";
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-  // output data of each row
-  while($row = $result->fetch_assoc()) {
-    echo "id: " . $row["idCliente"]. " - Name: " . $row["NombreCompania"]. " " . $row["NombreContacto"]. "<br>";
+  // Check connection
+  if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
   }
-} else {
-  echo "0 results";
-}
-$conn->close();
+  echo "Connected successfully";
+
+  $sql = "SELECT * FROM clientes";
+  $result = $conn->query($sql);
+
+  if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+      echo "id: " . $row["idCliente"]. " - Name: " . $row["NombreCompania"]. " " . $row["NombreContacto"]. "<br>";
+    }
+  } else {
+    echo "0 results";
+  }
+  $conn->close();
 ?>
--- semana1-conceptosbasicos/perltest.cgi
+++ semana1-conceptosbasicos/perltest.cgi
@@ -6,4 +6,4 @@ print '<meta name="author" content="Kay Vogelgesang">';
 print '<link href="/xampp/xampp.css" rel="stylesheet" type="text/css">';
 print '</head>';
 print "<body>&nbsp;<p><h1>GCI with MiniPerl</h1>";
-print "<p>CGI with MiniPerl is ready! ...</p></body></html>";
+print "<p>CGI with MiniPerl is ready! ...</p><p>Inside docker</p></body></html>";
